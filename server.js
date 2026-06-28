895b7503430002daa3edc3d086ea42c226107558
euronymous0713 <hayato20020713@icloud.com>
2026-06-29 03:02:11 +0900

    title screen

--- server.js
+++ server.js
@@ -1485,7 +1485,7 @@ function setupTurnTimerIfNeeded(roomId) {
         clearTurnTimer(r);
         moveToNextAliveTurn(r.game);
         finishGameIfNeeded(roomId);
-    }, TURN_DURATION_MS + 2000);
+    }, TURN_DURATION_MS + 3000);
 }
 
 function finishGameIfNeeded(roomId) {
